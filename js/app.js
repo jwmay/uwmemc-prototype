1c9df523f1622f8f2a23df7872fa5da91d879370
Joseph May <jwmay87@gmail.com>
2017-11-12 22:21:38 -0800

    merge in version 2

--- js/app.js
+++ js/app.js
@@ -41,38 +41,15 @@ $(function() {
   ////
   // Initialize the QD image slider.
   ////
-  var qdSlider = new Foundation.Orbit($('.qd-slider'), {
+  var qdSlider = new Foundation.Orbit($('.headline-slider'), {
     animInFromLeft: 'fade-in',
     animInFromRight: 'fade-in',
     animOutToLeft: 'fade-out',
     animOutToRight: 'fade-out',
-    containerClass: 'qd-slider-container',
-    slideClass: 'qd-slider-slide',
-    boxOfBullets: 'qd-slider-bullets',
-    nextClass: 'qd-slider-next',
-    prevClass: 'qd-slider-prev',
-  });
-
-  // Update left and right images when slide advances.
-  $('.qd-slider').on('slidechange.zf.orbit', function() {
-    var $active = $('.is-active');
-    var $first = $('.qd-slider-container').children().first();
-    var $last = $('.qd-slider-container').children().last();
-    var $left = $('.qd-slider-left img').fadeOut(1);
-    var $right = $('.qd-slider-right img').fadeOut(1);
-
-    // The first slide will have no previous child, so use the last slide.
-    var $previous = $active.prev().children('img').attr('src');
-    if ($previous === undefined) {
-      $previous = $last.children('img').attr('src');
-    }
-    $left.attr('src', $previous).delay(300).fadeIn(800);
-
-    // The last slide will have no next child, so use the first slide.
-    var $next = $active.next().children('img').attr('src');
-    if ($next === undefined) {
-      $next = $first.children('img').attr('src');
-    }
-    $right.attr('src', $next).delay(300).fadeIn(800);
+    containerClass: 'headline-slider-container',
+    slideClass: 'headline-slider-slide',
+    boxOfBullets: 'headline-slider-bullets',
+    nextClass: 'headline-slider-next',
+    prevClass: 'headline-slider-previous',
   });
 });
